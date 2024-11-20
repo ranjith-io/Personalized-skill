d23f3461294d84c0be7860f91c3266b695b1397c
RANJITH A <ranjithwebwiz@gmail.com>
2024-11-20 23:13:10 +0530

    updated

--- arrays/remove_even_evenplace.c
+++ arrays/remove_even_evenplace.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
-int distance(int arr[],int size){
-    // int c;
-    // int count[size];
-    for (int i=2;i<size;i++){
-        
+int evenplacerem(int arr[],int size){
+    
+    for (int i=2;i<size;i+=2){
+            if (arr[i]%2==0){
             for (int j=i;j<size;j++){
-            if (arr[j]%2==0)
-            {
-                printf("%dj",arr[j]);
-                arr[j]=arr[j+1];
+            
+                arr[i]=arr[i+1];
+
             }
             size--;
-            j--;
+            i--;
             
         }
     }
-        // count[i]=c;
-
-    
+   
     for (int i=0;i<size;i++){
         printf("%d ",arr[i]);
     }
 }
 int main() {
-    int arr[5]={8,1,9,4,3};
+    int arr[5]={8,1,9,49,30};
     int size=5;
     for (int i=0;i<size;i++){
         printf("%d ",arr[i]);
     }
     printf("\n");
     
-distance(arr,size);
+evenplacerem(arr,size);
 
     return 0;
 }
